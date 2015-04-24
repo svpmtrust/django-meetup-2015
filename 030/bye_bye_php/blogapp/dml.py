175f6f2db7acff9556290aa6529d8db3ca93005f
Chirag Choudhary <chirag200666@gmail.com>
2015-04-25 03:13:32 +0530

    modifying dml brief

--- 030/bye_bye_php/blogapp/dml.py
+++ 030/bye_bye_php/blogapp/dml.py
@@ -3,4 +3,4 @@ from blogapp.models import *
 author1 = Author.objects.create(name='Eswar')
 author2 = Author.objects.create(name='Chirag')
 BlogPost.objects.create(author=author1,  title='Hello World', brief='Say hi to python and bye-bye to php.', body='Welcome to the meetup')
-BlogPost.objects.create(author=author2,  title='Django is powerful', brief='Django allows you to do things fast and still have clean and pragmatic design.', body='Disqus, Instagram and pinterest have been built with Django')
+BlogPost.objects.create(author=author2,  title='Django is powerful', brief='Django allows you to build things fast and still have clean and pragmatic design.', body='Disqus, Instagram and pinterest have been built with Django')
